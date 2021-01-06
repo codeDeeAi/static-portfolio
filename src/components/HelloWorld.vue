0d10b7656c45f66dd8aaa8aad857f8e01bb7e3f7
Adeola <dunsin.bada@gmail.com>
2021-01-06 20:22:28 +0100

    projects added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -38,10 +38,10 @@
                <div>
                   <p class="text-center py-3 h5">Lets be social</p>
                   <ul class="flex justify-between container">
-                      <li><a class="btn border-purple-400 py-1 px-2 shadow hover:bg-purple-400 hover:text-white animate-pulse" target="_blank" href="linkedin.com/in/adeola-bada-aiism-a51174124"><i class="fab fa-linkedin fa-2x mr-auto"></i> </a></li>
-                      <li><a class="btn border-purple-400 py-1 px-2 shadow hover:bg-purple-400 hover:text-white animate-pulse" target="_blank" href="https://instagram.com/mr_.dee" ><i class="fab fa-instagram fa-2x mr-auto"></i> </a></li>
-                      <li><a class="btn border-purple-400 py-1 px-2 shadow hover:bg-purple-400 hover:text-white animate-pulse" target="_blank" href="https://twitter.com/adeolabada1"><i class="fab fa-twitter fa-2x mr-auto"></i></a></li>
-                      <li><a class="btn border-purple-400 py-1 px-2 shadow hover:bg-purple-400 hover:text-white animate-pulse" target="_blank" href="https://github.com/codeDeeAi"><i class="fab fa-github fa-2x mr-auto"></i></a></li>
+                      <li><a class="btn border-indigo-400 py-1 px-2 shadow hover:bg-indigo-400 hover:text-white animate-pulse" target="_blank" href="linkedin.com/in/adeola-bada-aiism-a51174124"><i class="fab fa-linkedin fa-2x mr-auto"></i> </a></li>
+                      <li><a class="btn border-indigo-400 py-1 px-2 shadow hover:bg-indigo-400 hover:text-white animate-pulse" target="_blank" href="https://instagram.com/mr_.dee" ><i class="fab fa-instagram fa-2x mr-auto"></i> </a></li>
+                      <li><a class="btn border-indigo-400 py-1 px-2 shadow hover:bg-indigo-400 hover:text-white animate-pulse" target="_blank" href="https://twitter.com/adeolabada1"><i class="fab fa-twitter fa-2x mr-auto"></i></a></li>
+                      <li><a class="btn border-indigo-400 py-1 px-2 shadow hover:bg-indigo-400 hover:text-white animate-pulse" target="_blank" href="https://github.com/codeDeeAi"><i class="fab fa-github fa-2x mr-auto"></i></a></li>
                   </ul> 
                </div>
                 <!-- Connect with me ends -->
@@ -78,27 +78,26 @@
             <!-- Project Header Ends -->
             <!-- Project Content -->
             <div class="justify-around grid md:grid-cols-2 sm:grid-cols-1">
-              <div class="rounded-tr-3xl rounded-bl-3xl hover:shadow project-box">
+              <div v-for="(pro, i) in  projects" :key="i" class="rounded-tr-3xl rounded-bl-3xl hover:shadow project-box">
                 <div class="container py-3">
                     <div class="project-description-box py-4">
                       <div class="main-box shadow rounded-bl-3xl rounded-tr-3xl bg-indigo-400 py-6 px-4">
-                        <p class="text-lg font-bold py-2">Title</p>
+                        <p class="text-lg font-bold py-2">{{pro.title}}</p>
                         <div class="shadow-inner rounded my-2 py-2">
-                          lasdoasdmasdasdd
+                          {{pro.details}}
                         </div>
                       </div>
-                      <div class="content-box shadow rounded-bl-3xl rounded-tr-3xl bg-gray-400">
-                        <ul class="list-inside list-disc">
-                          <li>HTML 5</li>
-                          <li>CSS 3</li>
-                          <li>JavaScript</li>
+                      <div class="content-box shadow rounded-bl-3xl rounded-tr-3xl bg-gray-400 py-4">
+                        <p class="text-center py-2">Top Language Used</p>
+                        <ul class="list-inside list-disc px-6">
+                          <li v-for="(topL, t) in pro.languages" :key="t">{{topL}}</li>
                       </ul>
                       </div>
                     </div>
                     <div class="project-options-box grid grid-cols-2 justify-between py-1 pb-2">
                       <div class="flex justify-start">
-                        <a href="#!" class="hover:bg-indigo-400 hover:text-white shadow-md mx-2 py-2 px-4 rounded-full">GitHub</a>
-                        <a href="#!" class="hover:bg-indigo-400 hover:text-white shadow-md mx-2 py-2 px-4 rounded-full">Web</a>
+                        <a v-if="pro.git" :href="pro.git" class="hover:bg-indigo-400 hover:text-white shadow-md mx-2 py-2 px-4 rounded-full">GitHub</a>
+                        <a v-if="pro.web" :href="pro.web" class="hover:bg-indigo-400 hover:text-white shadow-md mx-2 py-2 px-4 rounded-full">Web</a>
                       </div>
                     </div>
                 </div>
@@ -110,11 +109,11 @@
             <div id="contact" class="">
                 <div class="container md:px-20 sm:px-10 my-4 shadow-inner py-4">
                   <ul class="flex justify-between container">
-                      <li><a class="btn border-purple-400 py-1 px-2 shadow hover:bg-purple-400 hover:text-white animate-pulse" target="_blank" href="linkedin.com/in/adeola-bada-aiism-a51174124"><i class="fab fa-linkedin fa-2x mr-auto"></i> </a></li>
-                      <li><a class="btn border-purple-400 py-1 px-2 shadow hover:bg-purple-400 hover:text-white animate-pulse" target="_blank" href="https://instagram.com/mr_.dee" ><i class="fab fa-instagram fa-2x mr-auto"></i> </a></li>
-                      <li><a class="btn border-purple-400 py-1 px-2 shadow hover:bg-purple-400 hover:text-white animate-pulse" target="_blank" href="https://twitter.com/adeolabada1"><i class="fab fa-twitter fa-2x mr-auto"></i></a></li>
-                      <li><a class="btn border-purple-400 py-1 px-2 shadow hover:bg-purple-400 hover:text-white animate-pulse" target="_blank" href="https://github.com/codeDeeAi"><i class="fab fa-github fa-2x mr-auto"></i></a></li>
-                      <li><a class="btn border-purple-400 py-1 px-2 shadow hover:bg-purple-400 hover:text-white animate-pulse" target="_blank" href="mailto:dunsin.bada@gmail.com" ><i class="far fa-envelope fa-2x mr-auto"></i></a> </li>
+                      <li><a class="btn border-indigo-400 py-1 px-2 shadow hover:bg-indigo-400 hover:text-white animate-pulse" target="_blank" href="linkedin.com/in/adeola-bada-aiism-a51174124"><i class="fab fa-linkedin fa-2x mr-auto"></i> </a></li>
+                      <li><a class="btn border-indigo-400 py-1 px-2 shadow hover:bg-indigo-400 hover:text-white animate-pulse" target="_blank" href="https://instagram.com/mr_.dee" ><i class="fab fa-instagram fa-2x mr-auto"></i> </a></li>
+                      <li><a class="btn border-indigo-400 py-1 px-2 shadow hover:bg-indigo-400 hover:text-white animate-pulse" target="_blank" href="https://twitter.com/adeolabada1"><i class="fab fa-twitter fa-2x mr-auto"></i></a></li>
+                      <li><a class="btn border-indigo-400 py-1 px-2 shadow hover:bg-indigo-400 hover:text-white animate-pulse" target="_blank" href="https://github.com/codeDeeAi"><i class="fab fa-github fa-2x mr-auto"></i></a></li>
+                      <li><a class="btn border-indigo-400 py-1 px-2 shadow hover:bg-indigo-400 hover:text-white animate-pulse" target="_blank" href="mailto:dunsin.bada@gmail.com" ><i class="far fa-envelope fa-2x mr-auto"></i></a> </li>
                   </ul> 
                 </div>
             </div>
@@ -139,16 +138,105 @@ export default {
     return {
       copyright: " All rights reserved | Designed by ",
       name: "Bada Adeola O.",
-      eachProject: null,
+      projects:[
+                {
+                    title: 'Vue Js Windows History Buttons',
+                    details: "A highly customizable Vue Js 3 plugin for controlling browser history pages (next and previous)",
+                    languages:['JavaScript', 'Vue Js'],
+                    git:'https://github.com/codeDeeAi/vue-window-history-control-buttons.git',
+                    web:''
+                },
+                {
+                    title: 'Get Jokes Page',
+                    details: "A simple webpage with  HTML, CSS, Tailwind Css, JavaScript, and Vue Js that gets jokes from an open source API based on the user's filters",
+                    languages:['JavaScript', 'Vue Js'],
+                    git:'https://github.com/codeDeeAi/get_jokes_from_open_source_api.git',
+                    web:''
+                },
+                {
+                    title: 'Portfolio (WebApp)',
+                    details: "My new Portfolio designed with Laravel 8 and Vue Js , to showcase some of the works done by me",
+                    languages:['Laravel 8', 'Vue Js', 'Sql'],
+                    git:'https://github.com/codeDeeAi/New-Portfolio.git',
+                    web:''
+                },
+                {
+                    title: 'OneHourToDo Application',
+                    details: "A Project Based on a challenge to build a ToDo App with Laravel and Vue Js unde One Hour",
+                    languages:['Laravel 8', 'Vue Js', 'Sql'],
+                    git:'https://github.com/codeDeeAi/ToDoApp.git',
+                    web:''
+                },
+                {
+                    title: 'LaraBlog',
+                    details: "      A Blog web app with Laravel and Vue Js, it has some very important and unique features, most importantly it can be fully customized by the administrator. It also features user roles and permission at the admin panel",
+                    languages:['Laravel 7', 'Vue Js', 'Sql'],
+                    git:'https://github.com/codeDeeAi/Larablog.git',
+                    web:''
+                },
+                {
+                    title: 'TFLM',
+                    details: "An NGO and Ministry web app with Php and Javascript",
+                    languages:['JavaScript', 'Php', 'Sql'],
+                    git:'https://github.com/codeDeeAi/TFLM.git',
+                    web:''
+                },
+                {
+                    title: 'KYC Loan App',
+                    details: "An app designed with Php, which shows the simple process of loan verification of a bank XYZ",
+                    languages:['JavaScript', 'Php', 'Sql'],
+                    git:'https://github.com/codeDeeAi/kyc-loan-app.git',
+                    web:''
+                },
+               {
+                    title: 'RyseHub',
+                    details: "A simple functional e-commerce web app with just HTML, CSS, bootstrap, JavaScript, VueJS and Firebase ",
+                    languages:['JavaScript', 'Vue Js', 'Firebase'],
+                    git:'',
+                    web:'http://www.rysehub.com.ng'
+                },
+                {
+                    title: 'Twitter',
+                    details: "A simple recreation of twitter's landing page with just HTML, CSS, bootstrap and Simple JavaScript",
+                    languages:['JavaScript'],
+                    git:'https://github.com/codeDeeAi/Twitter.git',
+                    web:''
+                },
+                {
+                    title: 'Diamond Bridge of Desmond',
+                    details: "A simple webpage with  HTML, CSS, bootstrap, JavaScript, Vue Js and Materialize CSS ",
+                    languages:['JavaScript', 'Vue Js'],
+                    git:'https://github.com/codeDeeAi/Diamond-Bridge-.git',
+                    web:'https://www.diamondbridgeofdesmond.com'
+                },
+                {
+                    title: 'Bwort',
+                    details: "A simple landing page for a laundry business with just HTML, CSS, bootstrap and JavaScript",
+                    languages:['JavaScript'],
+                    git:'https://github.com/codeDeeAi/Bwort.git',
+                    web:''
+                },
+                {
+                    title: 'Sunview',
+                    details: "A simple landing page with just HTML, CSS, bootstrap and Simple JavaScript. Created to show the different ways carousels and grids can be used",
+                    languages:['JavaScript'],
+                    git:'https://github.com/codeDeeAi/sunny.git',
+                    web:''
+                },
+                {
+                    title: 'My Old Portfolio',
+                    details: "A landing page with  HTML, CSS , bootstrap, Simple JavaScript and VueJS. Created to show the various designs made by me",
+                    languages:['JavaScript', 'Vue Js'],
+                    git:'https://github.com/codeDeeAi/Portfolio.git',
+                    web:''
+                },
+      ]
     };
   },
   methods: {
     cur() {
       return new Date().getFullYear();
     },
-    openModal(){
-      this.eachProject = 'my Project'
-    }
   },
   created() {},
   mounted() {},
@@ -202,7 +290,6 @@ export default {
   width: 100%;
   height: 100%;
   background: #333;
-  display: flex;
   justify-content: center;
   align-items: center;
   transform-origin: bottom;
